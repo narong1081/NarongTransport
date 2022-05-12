6fa7a15b1ce68695c9c5270a3e78c4e98bd93891
narong <narong@diserve.co.th>
2022-05-12 16:45:17 +0700

    location lat lng end training 12may

--- lib/bodies/get_location.dart
+++ lib/bodies/get_location.dart
@@ -4,6 +4,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:narong_transport/utilities/my_dialog.dart';
 
 class GetLocationShowMap extends StatefulWidget {
@@ -78,12 +79,28 @@ class _GetLocationShowMapState extends State<GetLocationShowMap> {
     return position!;
   }
 
+  Set<Marker> myMarker() {
+    return <Marker>[
+      Marker(
+        markerId: MarkerId('id'),
+        position: LatLng(lat!, lng!),
+      ),
+    ].toSet();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: load
           ? Center(child: CircularProgressIndicator())
-          : Text('This Get Location\nlat : $lat \nlng : $lng'),
+          : GoogleMap(
+              initialCameraPosition: CameraPosition(
+                target: LatLng(lat!, lng!),
+                zoom: 16,
+              ),
+              onMapCreated: (value) {},
+              markers: myMarker(),
+            ),
     );
   }
 }
